52238e1f2ff0af3bd356a4a224080fe8035be6b0
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-02-14 14:20:59 +0000

    Styled the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,81 @@ import './App.css';
 import React, {useState} from 'react'
 import Main from './containers/Main/Main';
 import NavBar from './containers/NavBar/NavBar';
-
 import BeersData from './data/beers';
-// import CardList from './components/CardList/CardList'
-// import BeerCard  from './components/BeerCard/BeerCard';
+
+// useEffect(() => {
+//   fetch("https://api.punkapi.com/v2/beers?per_page=30")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       setData(data);
+//     });
+// }, []);
 
 
 function App() {
   const [beers, setBeers] = useState(BeersData);
+  const [filteredBeers, setFilteredBeers] = useState(BeersData);
 
 
   const highABV = (event) => {
     if (event.target.checked){
       const filteredHigh = beers.filter(beer => beer.abv > 6);
-      setBeers(filteredHigh); 
+      setFilteredBeers(filteredHigh); 
     }else {
-      setBeers(BeersData);
+      setFilteredBeers(BeersData);
     }
   }
 
-  
   const classicRange = (event) => {
     if (event.target.checked){
-    const filteredClassic = beers.filter(beer => beer.first_brewed >= 2010); 
-    setBeers(filteredClassic);
+      const filteredClassic = beers.filter(beer => beer.first_brewed.split("/")[1] <= 2010); 
+      setFilteredBeers(filteredClassic);
     }else {
-      setBeers(BeersData);
+      setFilteredBeers(BeersData);
     }
   }
 
   const acidicPH = (event) => {
-    if (event.target.checked){
-    const filteredAcidic = beers.filter(beer => beer.ph < 4);
-    setBeers(filteredAcidic);
+      if (event.target.checked){
+      const filteredAcidic = beers.filter(beer => beer.ph < 4);
+      setFilteredBeers(filteredAcidic);
     }else {
-      setBeers(BeersData);
+      setFilteredBeers(BeersData);
     }
   }
 
   const searchFilter = (event) => {
     console.log(event.target.value)
-    if (event.target.value === "") {
-      setBeers(BeersData);
+    
+    if (event.target.value) {
+      const searchName = beers.filter(beer => beer.name.toLowerCase().includes(event.target.value.toLowerCase()));
+      setFilteredBeers(searchName);
+
     }else {
-      const searchName = beers.filter(beer => beer.name.toLowerCase().indexOf(event.target.value) !== -1);
-      setBeers(searchName);
+      setFilteredBeers(BeersData);
     }
   }
+
+  // const filteredBeerList = data.filter((beer) => {
+  //   if (userInput) {
+  //     return beer.name.toLowerCase().includes(userInput);
+  //   }
+  //   if (!abv && !ph && !range) {
+  //     return data;
+  //   }
+  //   if (abv) {
+  //     return beer.abv > 6;
+  //   }
+  //   if (ph) {
+  //     return beer.ph < 4;
+  //   }
+  //   if (range) {
+  //     return beer.first_brewed.split("/")[1] >= 2010;
+  //   }
+  // });
+
 
   return (
     <div className="App">
@@ -57,7 +85,7 @@ function App() {
         {name:'Classic Range', filteredAlcohol:(event) => classicRange(event) },
         {name:'Acidic (ph < 4)', filteredAlcohol:(event) => acidicPH(event)}
         ]}/>
-      <Main beersArr={beers}/>      
+      <Main beersArr={filteredBeers}/>      
     </div>
   );
 }
